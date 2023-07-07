c74c13948e037f8dabd9381037a8ecfb26e14300
Iteratum <103502686+Iteratum@users.noreply.github.com>
2023-07-07 23:58:17 +0100

    modifying admin, views, and serializers

--- app/views.py
+++ app/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from .models import Blog
+from .serializer import BlogSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+@api_view(['GET', 'POST'])
+def BlogHost(request, format=None):
+    if request.method == 'GET':
+        blog = Blog.objects.all()
+        serialize = BlogSerializer(blog, many=True)
+        return Response(serialize.data)
