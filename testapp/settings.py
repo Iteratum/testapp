aff70da9b62b4140fad8c305ab655760c0f0e593
Iteratum <emmaogbo2002@gmail.com>
2023-07-22 11:05:42 -0700

    adding allowed origins and hosts

--- testapp/settings.py
+++ testapp/settings.py
@@ -26,7 +26,11 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://testapp1-7tdo.onrender.com/']
+
+CORS_ALLOWED_ORIGINS = [
+    "https://vercel.com/iteratum/r-blog-jeyl",
+]
 
 
 # Application definition
