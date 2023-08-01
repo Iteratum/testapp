7b84c5fb2087b379d82a33366b107df862734b82
Iteratum <emmaogbo2002@gmail.com>
2023-08-01 10:20:55 -0700

    setting debug to false

--- testapp/settings.py
+++ testapp/settings.py
@@ -24,9 +24,9 @@ SECRET_KEY = config('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=True, cast=bool)
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['.onrender.com', '*']
+ALLOWED_HOSTS = ['.onrender.com']
 
 CORS_ALLOWED_ORIGINS = [
     "https://r-blog-jeyl.vercel.app",
